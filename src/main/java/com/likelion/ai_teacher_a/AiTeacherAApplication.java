7ffb5180f6e0766e09e3f6a31747cb81189310c7
Jong Min Choi <choix531@crk.umn.edu>
2025-07-03 11:27:40 +0900

    Chore: 프로젝트 초기 설정

---
+++ src/main/java/com/likelion/ai_teacher_a/AiTeacherAApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.ai_teacher_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTeacherAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTeacherAApplication.class, args);
+	}
+
+}
