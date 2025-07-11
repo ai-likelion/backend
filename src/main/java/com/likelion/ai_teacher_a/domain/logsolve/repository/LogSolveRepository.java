0ac7ae880ae1dad490bc695ea389cd3e009ab3fc
Yejeong, Ham <dev@thelightway.kr>
2025-07-11 09:46:52 +0900

    Revert "Feature: OpenAI 연동 추가(#7)" (#10)
    This reverts commit 0335b3dfeac193d073e4400b2f6930867ab2a163.

--- src/main/java/com/likelion/ai_teacher_a/domain/logsolve/repository/LogSolveRepository.java
+++
@@ -1,9 +0,0 @@
-package com.likelion.ai_teacher_a.domain.logsolve.repository;
-
-import com.likelion.ai_teacher_a.domain.logsolve.entity.LogSolve;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LogSolveRepository extends JpaRepository<LogSolve, Long> {
-}
