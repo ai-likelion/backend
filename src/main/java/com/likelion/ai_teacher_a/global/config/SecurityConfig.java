cece7bdeeafdca0a5147b4f26dbc77524c4b718e
thelightway <dev@thelightway.kr>
2025-07-17 01:10:18 +0900

    Refactor: SecurityConfig 변경 및 CORS 문제 관련 webconfig 삭제

--- src/main/java/com/likelion/ai_teacher_a/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/ai_teacher_a/global/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.likelion.ai_teacher_a.global.config;
 
+import com.likelion.ai_teacher_a.global.auth.CustomOAuth2UserService;
 import com.likelion.ai_teacher_a.global.auth.JwtAuthenticationFilter;
+import com.likelion.ai_teacher_a.global.auth.OAuth2AuthenticationSuccessHandler;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,16 +20,25 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll()
-                        .anyRequest().authenticated()
+            .csrf(CsrfConfigurer<HttpSecurity>::disable)
+            .cors(CorsConfigurer<HttpSecurity>::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login.html", "/mypage.html", "/success.html", "/refresh_test.html", "/login/oauth2/code/kakao", "/oauth2/authorization/kakao", "/api/auth/refresh").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .userInfoEndpoint(userInfo -> userInfo
+                    .userService(customOAuth2UserService)
                 )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .successHandler(oAuth2AuthenticationSuccessHandler)
+            )
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
     }
 }
