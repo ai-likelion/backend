107b9eb561ea300a0116b48ec0bca24c29c8a1ca
thelightway <dev@thelightway.kr>
2025-07-17 04:36:53 +0900

    Fix: CORS 설정 변경

--- src/main/java/com/likelion/ai_teacher_a/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/ai_teacher_a/global/config/SecurityConfig.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.likelion.ai_teacher_a.global.auth.filter.JwtAuthenticationFilter;
 import com.likelion.ai_teacher_a.global.auth.handler.OAuth2AuthenticationSuccessHandler;
@@ -24,12 +24,13 @@ public class SecurityConfig {
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 	private final CustomOAuth2UserService customOAuth2UserService;
 	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+	private final CorsConfigurationSource corsConfigurationSource;
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		return http
 			.csrf(CsrfConfigurer<HttpSecurity>::disable)
-			.cors(CorsConfigurer<HttpSecurity>::disable)
+			.cors(cors -> cors.configurationSource(corsConfigurationSource))
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers(
 					"/",
