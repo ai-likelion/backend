f1da1f0e69728460437a285b4b2fde0ed4fa180e
thelightway <dev@thelightway.kr>
2025-07-17 03:31:13 +0900

    Feat: Front-end 연동을 위한 테스트 페이지 추가

---
+++ src/main/java/com/likelion/ai_teacher_a/global/auth/controller/TempUserController.java
@@ -0,0 +1,32 @@
+package com.likelion.ai_teacher_a.global.auth.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.likelion.ai_teacher_a.domain.user.dto.UserResponseDto;
+import com.likelion.ai_teacher_a.domain.user.entity.User;
+import com.likelion.ai_teacher_a.domain.user.repository.UserRepository;
+import com.likelion.ai_teacher_a.global.auth.CustomUserDetails;
+import com.likelion.ai_teacher_a.global.auth.resolver.annotation.LoginUserId;
+
+import lombok.RequiredArgsConstructor;
+
+//TODO: 추후 삭제해야하는 클래스 테스트 목적 클래스 임.
+@RestController
+@RequestMapping("/test/api/temp/user")
+@RequiredArgsConstructor
+public class TempUserController {
+
+	private final UserRepository userRepository;
+
+	@GetMapping("/me")
+	public ResponseEntity<UserResponseDto> getLoggedInUserInfo(@LoginUserId Long userId) {
+		User user = userRepository.findById(userId)
+			.orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다"));
+		return ResponseEntity.ok(UserResponseDto.from(user));
+	}
+}
