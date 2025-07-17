8c1636e7886166083f47299e312a7b6f13cfcdc0
thelightway <dev@thelightway.kr>
2025-07-17 23:49:37 +0900

    Chore: LoginUserId 파라미터 Swagger 추가

--- src/main/java/com/likelion/ai_teacher_a/global/auth/resolver/annotation/LoginUserId.java
+++ src/main/java/com/likelion/ai_teacher_a/global/auth/resolver/annotation/LoginUserId.java
@@ -6,8 +6,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Parameter(
+	name = "Authorization",
+	in = ParameterIn.HEADER,
+	description = "Bearer {Access 토큰}",
+	schema= @Schema(type="string"),
+	required = true
+)
 public @interface LoginUserId {
 }
