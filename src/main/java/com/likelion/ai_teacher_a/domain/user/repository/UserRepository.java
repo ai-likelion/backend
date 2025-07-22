e496b1800b4fb57dd544d666a5daab00e642ddeb
thelightway <dev@thelightway.kr>
2025-07-22 12:09:31 +0900

    Fix: 회원 로그인 가능하도록 수정

--- src/main/java/com/likelion/ai_teacher_a/domain/user/repository/UserRepository.java
+++ src/main/java/com/likelion/ai_teacher_a/domain/user/repository/UserRepository.java
@@ -1,10 +1,19 @@
 package com.likelion.ai_teacher_a.domain.user.repository;
 
-import com.likelion.ai_teacher_a.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
+import com.likelion.ai_teacher_a.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+
+	@Modifying
+	@Query(value = """
+		DELETE FROM log_solve WHERE user_id = :userId;
+		DELETE FROM user_jr WHERE user_id = :userId;
+		DELETE FROM image WHERE user_id = :userId;
+		DELETE FROM users WHERE id = :userId;
+		""", nativeQuery = true)
+	void deleteById(Long userId);
 }
--- src/main/java/com/likelion/ai_teacher_a/domain/user/service/UserService.java
+++ src/main/java/com/likelion/ai_teacher_a/domain/user/service/UserService.java
@@ -3,12 +3,15 @@ package com.likelion.ai_teacher_a.domain.user.service;
 import org.springframework.stereotype.Service;
 
 import com.likelion.ai_teacher_a.domain.image.repository.ImageRepository;
+import com.likelion.ai_teacher_a.domain.logsolve.repository.LogSolveRepository;
 import com.likelion.ai_teacher_a.domain.user.entity.User;
 import com.likelion.ai_teacher_a.domain.user.repository.UserRepository;
+import com.likelion.ai_teacher_a.domain.userJr.repository.UserJrRepository;
 import com.likelion.ai_teacher_a.global.auth.util.JwtUtil;
 import com.likelion.ai_teacher_a.global.exception.CustomException;
 import com.likelion.ai_teacher_a.global.exception.ErrorCode;
 
+import jakarta.transaction.Transactional;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -21,12 +24,16 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final ImageRepository imageRepository;
+	private final UserJrRepository userJrRepository;
+	private final LogSolveRepository logSolveRepository;
 	private final JwtUtil jwtUtil;
 
+	@Transactional
 	public void deleteUserById(Long id) {
 		User user = userRepository.findById(id)
 			.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-		userRepository.delete(user);
+
+		userRepository.deleteById(user.getId());
 	}
 }
 
