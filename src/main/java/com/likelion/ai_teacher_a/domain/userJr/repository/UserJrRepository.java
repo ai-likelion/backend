1f5e60e875bcc497c46e2064f4c2539d7ad686ce
thelightway <dev@thelightway.kr>
2025-07-22 12:09:31 +0900

    Fix: 회원 로그인 가능하도록 수정

--- src/main/java/com/likelion/ai_teacher_a/domain/userJr/repository/UserJrRepository.java
+++ src/main/java/com/likelion/ai_teacher_a/domain/userJr/repository/UserJrRepository.java
@@ -1,34 +1,32 @@
 package com.likelion.ai_teacher_a.domain.userJr.repository;
 
-import com.likelion.ai_teacher_a.domain.user.entity.User;
-import com.likelion.ai_teacher_a.domain.userJr.entity.UserJr;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-import java.util.Optional;
+import com.likelion.ai_teacher_a.domain.userJr.entity.UserJr;
 
 public interface UserJrRepository extends JpaRepository<UserJr, Long> {
 
-    @Query("""
-            SELECT uj FROM UserJr uj
-            LEFT JOIN FETCH uj.user
-            LEFT JOIN FETCH uj.image
-            WHERE uj.user.id = :userId
-            ORDER BY uj.userJrId ASC""")
-    List<UserJr> findByUserIdFetchJoin(@Param("userId") Long userId);
+	@Query("""
+		SELECT uj FROM UserJr uj
+		LEFT JOIN FETCH uj.user
+		LEFT JOIN FETCH uj.image
+		WHERE uj.user.id = :userId
+		ORDER BY uj.userJrId ASC""")
+	List<UserJr> findByUserIdFetchJoin(@Param("userId") Long userId);
 
-    boolean existsByUserIdAndNickname(Long userId, String nickname);
+	boolean existsByUserIdAndNickname(Long userId, String nickname);
 
-    void deleteAllByUser(User user);
-
-    @Query("""
-                SELECT uj FROM UserJr uj
-                LEFT JOIN FETCH uj.user
-                LEFT JOIN FETCH uj.image
-                WHERE uj.userJrId = :userJrId
-            """)
-    Optional<UserJr> findByIdWithUserAndImage(@Param("userJrId") Long userJrId);
+	@Query("""
+		    SELECT uj FROM UserJr uj
+		    LEFT JOIN FETCH uj.user
+		    LEFT JOIN FETCH uj.image
+		    WHERE uj.userJrId = :userJrId
+		""")
+	Optional<UserJr> findByIdWithUserAndImage(@Param("userJrId") Long userJrId);
 
 }
