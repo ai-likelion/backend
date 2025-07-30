a5d432eb9a63f3dc84b040b7626faf4a77e5b5ed
thelightway <dev@thelightway.kr>
2025-07-30 23:57:00 +0900

    Fix: S3 저장소 -> R2로 변경

--- src/main/java/com/likelion/ai_teacher_a/domain/image/service/S3Uploader.java
+++ src/main/java/com/likelion/ai_teacher_a/domain/image/service/S3Uploader.java
@@ -4,9 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -20,28 +17,23 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3Uploader {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
+    private final S3Client s3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+
+    @Value("${cloud.aws.s3.public.url}")
+    private String r2PublicUrl;
 
 
     public String upload(MultipartFile file) throws IOException {
         String originalName = file.getOriginalFilename();
         String rawFileName = UUID.randomUUID() + "_" + originalName;
 
-        getS3Client().putObject(
+        s3Client.putObject(
                 PutObjectRequest.builder()
                         .bucket(bucket)
                         .key(rawFileName)
-                        .acl("public-read")
                         .contentType(file.getContentType())
                         .build(),
                 software.amazon.awssdk.core.sync.RequestBody.fromBytes(file.getBytes())
@@ -55,31 +47,20 @@ public class S3Uploader {
         String key = extractKeyFromUrl(fileUrl);
         System.out.println("🧹 Deleting S3 key: " + key); // 로그 확인용
 
-        getS3Client().deleteObject(builder ->
+        s3Client.deleteObject(builder ->
                 builder.bucket(bucket).key(key)
         );
-    }
-
-
-    private S3Client getS3Client() {
-        return S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(
-                        StaticCredentialsProvider.create(
-                                AwsBasicCredentials.create(accessKey, secretKey)
-                        )
-                ).build();
     }
 
     private String generatePublicUrl(String rawFileName) {
         String encodedFileName = URLEncoder.encode(rawFileName, StandardCharsets.UTF_8)
                 .replaceAll("\\+", "%20");
-        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + encodedFileName;
+        return r2PublicUrl + "/" + bucket + "/" + encodedFileName;
     }
 
 
     private String extractKeyFromUrl(String fileUrl) {
-        String baseUrl = "https://" + bucket + ".s3." + region + ".amazonaws.com/";
+        String baseUrl = r2PublicUrl + "/" + bucket + "/";
         String encodedKey = fileUrl.replace(baseUrl, "");
         return URLDecoder.decode(encodedKey, StandardCharsets.UTF_8);
     }
